d241e1ca4d09c4e0d3fe5a5a5bfb99438557c880
Xin Guo <emagrorrimxguo@gmail.com>
2019-05-23 18:23:20 +0800

    Init project via a hello world program

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send("Hello, World!");
+});
+
+app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send("About");
+});
+
+app.use((req, res) => {
+  res.type('jstext/plainon');
+  res.status(404);
+  res.send("404 - Not Found");
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
