d49648a17d26bd9cf3fecc8f09a57fe3b87bc91b
Xin Guo <emagrorrimxguo@gmail.com>
2019-05-28 17:21:45 +0800

    Include db name into mongodb url var

--- src/common/db/mongoose/db.js
+++ src/common/db/mongoose/db.js
@@ -1,17 +1,16 @@
 import mongoose from 'mongoose';
 
-const LOCAL_MONGODB_URI = 'mongodb://127.0.0.1:27017'
+const LOCAL_MONGODB_URI = 'mongodb://127.0.0.1:27017/cxmobile'
 const DB_NAME = 'cxmobile';
 
 export default class MongooseDatabase {
   constructor() {
-    const mongodbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
-    const databaseURI = mongodbURI + '/cxmobile';
-    mongoose.connect(databaseURI, { useNewUrlParser: true });
+    const mongodbURI = process.env.MONGODB_URI || LOCAL_MONGODB_URI;
+    mongoose.connect(mongodbURI, { useNewUrlParser: true });
     const conn = mongoose.connection;
     conn.on('error', console.error.bind(console, 'connection error:'));
     conn.once('open', () => {
-      console.log("Connect to MongoDB: " + databaseURI + ' successfully!')
+      console.log("Connect to MongoDB: " + mongodbURI + ' successfully!')
     });
     this._conn = conn;
   }
