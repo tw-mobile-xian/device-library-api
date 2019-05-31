4870d687611fe217b24a27985aa5cd7108d49b75
Xin Guo <emagrorrimxguo@gmail.com>
2019-05-31 11:50:45 +0800

    Add leading zero when time is less than 10

--- src/common/DateFormatter.js
+++ src/common/DateFormatter.js
@@ -3,9 +3,9 @@ export default class DateFormatter {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
+    const hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    const minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    const second = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
     return [year, month, day].join('-') + ' ' + [hour, minute, second].join(':');
   }
 }
